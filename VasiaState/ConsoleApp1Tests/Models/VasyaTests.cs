b049cbcaadfd4b788c56679f501ca89f5642921a
bulsond <bulsond@gmail.com>
2019-02-01 20:40:27 +0300

    Добавлен тестовый проект

---
+++ VasiaState/ConsoleApp1Tests/Models/VasyaTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models.Tests
+{
+    [TestClass()]
+    public class VasyaTests
+    {
+        [TestMethod()]
+        public void VasyaTestCtor()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FreezeWaterTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WalkOnWaterTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
