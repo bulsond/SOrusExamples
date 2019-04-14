e8351dc6081b2d72666ee79fd93b673f943c6e79
bulsond <bulsond@gmail.com>
2019-04-14 21:05:51 +0300

    +ConfBind - WinForms пример конфигурации с горячей клавишей

---
+++ ConfBind/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._comboBoxKeys = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // _comboBoxKeys
+            // 
+            this._comboBoxKeys.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this._comboBoxKeys.FormattingEnabled = true;
+            this._comboBoxKeys.Location = new System.Drawing.Point(62, 26);
+            this._comboBoxKeys.Name = "_comboBoxKeys";
+            this._comboBoxKeys.Size = new System.Drawing.Size(121, 21);
+            this._comboBoxKeys.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 95);
+            this.Controls.Add(this._comboBoxKeys);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox _comboBoxKeys;
+    }
+}
+
---
+++ ConfBind/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string _key;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //заполняем комбобокс
+            _comboBoxKeys.Items.AddRange(new string[] { "F5", "F6", "F7", "F8", "F9", "F10" });
+
+            //читаем из конфигурации значение горячей клавиши
+            _key = Properties.Settings.Default.StartButton;
+
+            //находим в комбобоксе выбранную ранее клавишу
+            if (!String.IsNullOrEmpty(_key))
+            {
+                _comboBoxKeys.SelectedIndex = _comboBoxKeys.Items.IndexOf(_key);
+            }
+
+            //подписываемся на событие изменения выбора
+            _comboBoxKeys.SelectedIndexChanged += ComboBoxKeys_SelectedIndexChanged;
+        }
+
+        //обработчик выбора в комбобоксе
+        private void ComboBoxKeys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //получаем выбранное значение
+            _key = _comboBoxKeys.Items[_comboBoxKeys.SelectedIndex].ToString();
+
+            //запоминаем в выбранное значение
+            Properties.Settings.Default.StartButton = _key;
+            Properties.Settings.Default.Save();
+
+            MessageBox.Show($"Вы выбрали клавишу: {_key}");
+        }
+
+        //переопределяем метод ловящий нажатия клавиш
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            //используем конвертер чтоб значение Keys преобразовать к строке
+            KeysConverter kc = new KeysConverter();
+            //если пользователь нажал выбранную клавишу
+            //запускаем метод связанный с ней
+            if (kc.ConvertToString(keyData) == _key)
+            {
+                RunMethodHotKey();
+                return true;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        //метод связанный с горячей клавишей
+        private void RunMethodHotKey()
+        {
+            MessageBox.Show($"Вы нажали клавишу: {_key}");
+        }
+    }
+}
